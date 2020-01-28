d0f0824765688255349fa423f8cb04e1b59083d3
Babkin Artyom <dr.v991149@gmail.com>
2020-01-28 14:45:23 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Invalidu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.table.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.table.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.table.BackgroundColor = System.Drawing.Color.White;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.table.Location = new System.Drawing.Point(12, 12);
+            this.table.Name = "table";
+            this.table.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.table.Size = new System.Drawing.Size(479, 441);
+            this.table.TabIndex = 0;
+            this.table.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.table.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.table_CellDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(416, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 502);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invalidu
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<DataGridViewCell> _selectedCells;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _selectedCells = new List<DataGridViewCell>();
+            table.ColumnCount = 20;
+            table.RowCount = 20;
+
+            table.ClearSelection();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = e.RowIndex;
+            var column = e.ColumnIndex;
+
+            var selectedCell = table[column, row];
+            table.ClearSelection();
+
+            if (!_selectedCells.Contains(selectedCell))
+            {
+                _selectedCells.Add(selectedCell);
+            }
+            else
+            {
+                _selectedCells.Remove(selectedCell);
+            }
+
+            _selectedCells.ForEach(c => c.Selected = true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = string.Empty;
+
+            var rows = _selectedCells.Select(c => 19 - c.RowIndex).ToList();
+            var columns = _selectedCells.Select(c => c.ColumnIndex).ToList();
+
+            for (int i = 0; i < _selectedCells.Count; i++)
+            {
+                var tmp = $"{{ {columns[i]}, {rows[i]} }}";
+                if (i < _selectedCells.Count - 1)
+                {
+                    tmp += ",\n";
+                }
+
+                result += tmp;
+            }
+
+            var file = File.CreateText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "file.txt"));
+
+            file.WriteLine(result);
+            file.Close();
+        }
+
+        private void table_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+    }
+}
